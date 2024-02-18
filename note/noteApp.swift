10fca4af9f708f3fc36d984f9b2a7119b501c112
giovannimhern <giovannimhern@uchicago.edu>
2024-02-17 18:11:47 -0600

    Initial Commit

---
+++ note/noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteApp.swift
+//  note
+//
+//  Created by Giovanni Maya on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
